015ca7d12098e50fb21680b9590bcc495cd59001
yuto.eguchi <gsh05196yu@yahoo.co.jp>
2020-09-10 10:30:14 +0900

    ec-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,22 @@
+import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireFunctionsModule, REGION } from '@angular/fire/functions';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +24,20 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireFunctionsModule,
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatAutocompleteModule,
   ],
-  providers: [],
+  providers: [
+    { provide: REGION, useValue: 'asia-northeast1' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAOE3ULSMK8Mx8ybbmbNKoCq17OF2ZOBfs",
+    authDomain: "ec-demo-51544.firebaseapp.com",
+    databaseURL: "https://ec-demo-51544.firebaseio.com",
+    projectId: "ec-demo-51544",
+    storageBucket: "ec-demo-51544.appspot.com",
+    messagingSenderId: "219077765425",
+    appId: "1:219077765425:web:ded83d4bf975917b486810",
+    measurementId: "G-NN8YTBHQNP"
+  }
 };
 
 /*
